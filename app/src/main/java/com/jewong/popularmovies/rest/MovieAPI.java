9f84d9e855417925279a01c7e555ad12265d1a90
jewong42 <jewong42@outlook.com>
2020-07-30 17:34:53 -0700

    Updated endpoints and fixed issue with details cutting off.

--- app/src/main/java/com/jewong/popularmovies/rest/MovieAPI.java
+++ app/src/main/java/com/jewong/popularmovies/rest/MovieAPI.java
@@ -4,12 +4,14 @@ import com.jewong.popularmovies.data.MovieList;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface MovieAPI {
 
-    @GET("3/discover/movie?")
-    Call<MovieList> getMovieByDiscover(@Query("api_key") String apiKey,
-                                       @Query("sort_by") String sortBy);
+    @GET("3/movie/{sort_by}/")
+    Call<MovieList> getMovieByDiscover(
+            @Path("sort_by") String sortBy,
+            @Query("api_key") String apiKey);
 
 }
--- app/src/main/java/com/jewong/popularmovies/rest/MovieAPIClient.java
+++ app/src/main/java/com/jewong/popularmovies/rest/MovieAPIClient.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.jewong.popularmovies.data.MovieList;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -18,18 +20,23 @@ public class MovieAPIClient {
     private MovieAPI mMovieAPI;
 
     public MovieAPIClient() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor()
+                .setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder()
+                .addInterceptor(logging);
         Gson gson = new GsonBuilder()
                 .setLenient()
                 .create();
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(httpClient.build())
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
         this.mMovieAPI = retrofit.create(MovieAPI.class);
     }
 
     public void getMovieByDiscover(String sortBy, Callback<MovieList> callback) {
-        mMovieAPI.getMovieByDiscover(API_KEY, sortBy)
+        mMovieAPI.getMovieByDiscover(sortBy, API_KEY)
                 .enqueue(new Callback<MovieList>() {
                     @Override
                     public void onResponse(Call<MovieList> call, Response<MovieList> response) {
